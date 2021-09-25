2a0ca89d9a8d68a8eabd3458cb4985a6d8facd3f
Seungwon Pang <legomaniac1005@gmail.com>
2021-09-26 02:48:57 +0900

    Cleaned Code

--- assignment.py
+++ assignment.py
@@ -1,5 +1,5 @@
 #############################
-univ_id = "0000"           ##
+univ_id = "0000"  ##
 #############################
 
 from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, LEFT, RIGHT, BOTTOM
@@ -11,7 +11,9 @@ MARGIN = 20  # Pixels around the board
 SIDE = 50  # Width of every board cell.
 WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
 DELAY = 0.05  # the delay time makes the changing number visible
-#PROBLEM_NUM = problem_set.problem_num
+
+
+# PROBLEM_NUM = problem_set.problem_num
 
 class SudokuUI(Frame):
     def __init__(self, parent):
@@ -23,12 +25,11 @@ class SudokuUI(Frame):
         self.parent.title("AI: Sudoku Solver")
         self.pack(fill=BOTH)
         self.canvas = Canvas(self, width=WIDTH, height=HEIGHT + 10)
-        self.easy_problem = Problem(self.canvas, 0, DELAY, random.randint(0, 1), setting = "easy")
-        self.hard_problem = Problem(self.canvas, 0, DELAY, random.randint(0, 1), setting = "hard")
+        self.easy_problem = Problem(self.canvas, 0, DELAY, random.randint(0, 1), setting="easy")
+        self.hard_problem = Problem(self.canvas, 0, DELAY, random.randint(0, 1), setting="hard")
 
-        self.problem = None # Set this by click button
-        
-        
+        self.problem = None  # Set this by click button
+
         # Initialize each cell in the puzzle
         for i in range(1, 10):
             for j in range(1, 10):
@@ -39,18 +40,18 @@ class SudokuUI(Frame):
         self.item = self.canvas.create_text(40, 490, text='Count :', fill="black", font=("Helvetica", 13))
         self.item = self.canvas.create_text(95, 490, text='', fill="black", font=("Helvetica", 13))
         self.item = self.canvas.create_text(170, 490, text='Average :', fill="black", font=("Helvetica", 13))
-        self.item = self.canvas.create_text(225, 490, text='',fill="black", font=("Helvetica", 13))
-        self.item = self.canvas.create_text(320, 490, text='Ranking :',fill="black", font=("Helvetica", 13))
+        self.item = self.canvas.create_text(225, 490, text='', fill="black", font=("Helvetica", 13))
+        self.item = self.canvas.create_text(320, 490, text='Ranking :', fill="black", font=("Helvetica", 13))
         self.item = self.canvas.create_text(370, 490, text='', fill="black", font=("Helvetica", 13))
         self.item = self.canvas.create_text(420, 490, text='Total :', fill="black", font=("Helvetica", 13))
         self.item = self.canvas.create_text(460, 490, text='', fill="black", font=("Helvetica", 13))
         self.canvas.pack(fill=BOTH, side=TOP)
         self.start_button1 = Button(self, text="__Hard__", command=self.__start_hard_solver)
         self.start_button2 = Button(self, text="__Easy__", command=self.__start_easy_solver)
-        
+
         self.start_button2.pack(side=LEFT)
         self.start_button1.pack(side=RIGHT)
-        #self.start_button2.config(state="disabled")
+        # self.start_button2.config(state="disabled")
         self.__draw_grid()
 
     # Draws 9x9 grid
@@ -67,42 +68,46 @@ class SudokuUI(Frame):
             x1 = WIDTH - MARGIN
             y1 = MARGIN + i * SIDE
             self.canvas.create_line(x0, y0, x1, y1, fill="black", width=width)
+
     def __start_easy_solver(self):
-      self.problem = self.easy_problem
-      self.__start_solver()
-      
+        self.problem = self.easy_problem
+        self.__start_solver()
+
     def __start_hard_solver(self):
-      self.problem = self.hard_problem
-      self.__start_solver()
-      
-      
+        self.problem = self.hard_problem
+        self.__start_solver()
+
     def __start_solver(self):
         self.start_button1.config(state="disabled")
         self.start_button2.config(state="disabled")
         for i in range(self.problem.problem_num):
             for m in range(1, 10):
                 for n in range(1, 10):
-                    if(self.problem.given_number[m-1][n-1] != 0): self.canvas.itemconfig(9 * (m - 1) + n, text=self.problem.given_number[m-1][n-1], tags="numbers", fill="blue")
-                    else:self.canvas.itemconfig(9 * (m - 1) + n, text='', tags="numbers", fill="black")
-                    
+                    if self.problem.given_number[m - 1][n - 1] != 0:
+                        self.canvas.itemconfig(9 * (m - 1) + n, text=self.problem.given_number[m - 1][n - 1],
+                                               tags="numbers", fill="blue")
+                    else:
+                        self.canvas.itemconfig(9 * (m - 1) + n, text='', tags="numbers", fill="black")
+
             self.solver_class = solver_class(self.problem)
             self.solver_class.solver()
-            if self.problem.finished==0:
+            if self.problem.finished == 0:
                 self.problem.fail()
                 return
             self.canvas.update()
-            
-            
-            if(i != self.problem.problem_num -1):  self.problem = Problem(self.canvas, self.problem.tk, 0.0, self.problem.temp, setting = self.problem.setting)
+
+            if i != self.problem.problem_num - 1:  self.problem = Problem(self.canvas, self.problem.tk, 0.0,
+                                                                          self.problem.temp,
+                                                                          setting=self.problem.setting)
         self.problem.update_a()
-        #self.start_button2.config(state="active")
-        
-        #If the problem has finished, this function will display "Finished!"
+        # self.start_button2.config(state="active")
+
+        # If the problem has finished, this function will display "Finished!"
         self.problem.is_done()
-        
+
     def __submit(self):
-        request=self.problem.submit(univ_id, "")
-        message=request.split(',')
+        request = self.problem.submit(univ_id, "")
+        message = request.split(',')
         if int(message[0]) == 100:
             self.problem.fail_10min()
         elif int(message[0]) == 101:
@@ -119,52 +124,23 @@ class SudokuUI(Frame):
             #   print "501"
             self.problem.wrong_id_pw()
 
+
 class solver_class():
     def __init__(self, problem):
+        self.puzzle = []
         self.problem = problem
         self.given_number = problem.given_number
 
     def solver(self):
-        self.puzzle = []
         new = []
         for i in range(0, 9):
             for j in range(0, 9):
-                if(self.given_number[i][j] != 0): new.append(self.given_number[i][j])
-                else: new.append(0)
+                if self.given_number[i][j] != 0:
+                    new.append(self.given_number[i][j])
+                else:
+                    new.append(0)
             self.puzzle.append(new)
             new = []
-
-        #AC-3(csp)
-            #while queue is not empty:
-                #(Xi,Xj) <- REMOVE-FIRST(queue):
-                #if REVISE(csp, Xi, Xj):
-                    #if size of Di = 0:
-                        #return false
-                    #for Xk in Xi. NEIGHBORS - {Xj}:
-                        #add (Xk, Xi) to queue
-
-        #REVISE(csp, Xi, Xj):
-            #revised = false
-            #for x in Di:
-                #if no value in Dj allows (x, y) to satisfy the constraint between Xi and Xj:
-                    #delete x from Di
-                    #revised = true
-            #return revised
-
-        #domain_set에서 순서는 : (1,1)=0, (1,2)=1, (1,3)=2, ..... (9,9)=80
-        '''
-        domains, domain_sizes = self.calc_domains(self.given_number)
-
-        for values in domain_sizes:
-            i = values[1]
-            j = values[2]
-            for k in domains[values[3]]:
-                if k != 0:
-                    output = self.problem.checker(i, j, k)  # Try to input 'K' & This increases the number of attempts
-                    if output == 1:  # if the value is correct, checker will output 1. Otherwise, output is 0.
-                        self.puzzle[i-1][j-1] = k
-                        break
-        '''
 
         for items in range(81):
             domains, domain_sizes = self.calc_domains(self.puzzle)
@@ -177,36 +153,41 @@ class solver_class():
                         self.puzzle[i - 1][j - 1] = k
                         break
 
-
     def calc_domains(self, given_number):
-    # i is row, j is column index
         domain_set = []
         domain_sizes = []
         count = 0
         for i in range(1, 10):
             for j in range(1, 10):
-                # select variable
                 if given_number[i - 1][j - 1] == 0:
-                    domain = list(range(1, 10))
-                    row_box = (int)((i - 1) // 3 + 1)
-                    column_box = (int)((j - 1) // 3 + 1)
-                    for item in range(1, 10):
-                        if given_number[i - 1][item - 1] in domain:
-                            domain.remove(int(given_number[i - 1][item - 1]))
-                        if given_number[item - 1][j - 1] in domain:
-                            domain.remove(int(given_number[item - 1][j - 1]))
-                    for x in range((row_box - 1) * 3 + 1, row_box * 3):
-                        for y in range((column_box - 1) * 3 + 1, column_box * 3):
-                            if given_number[x - 1][y - 1] in domain:
-                                domain.remove(int(given_number[x - 1][y - 1]))
+                    domain = self.calc_constraints(given_number, i, j)
                     domain_set.append(domain)
                     domain_sizes.append([len(domain), i, j, count])
                 else:
                     domain_set.append([0])
                     domain_sizes.append([10, i, j, count])
                 count += 1
-        domain_sizes.sort(key = lambda x: x[0])
+        domain_sizes.sort(key=lambda x: x[0])
         return domain_set, domain_sizes
+
+    def calc_constraints(self, given_number, i, j):
+        domain = list(range(1, 10))
+        row_box = int((i - 1) // 3 + 1)
+        column_box = int((j - 1) // 3 + 1)
+        for item in range(1, 10):
+            # Row binary constraints
+            if given_number[i - 1][item - 1] in domain:
+                domain.remove(int(given_number[i - 1][item - 1]))
+            # Columns binary constraints
+            if given_number[item - 1][j - 1] in domain:
+                domain.remove(int(given_number[item - 1][j - 1]))
+        for x in range((row_box - 1) * 3 + 1, row_box * 3):
+            # Box constraints
+            for y in range((column_box - 1) * 3 + 1, column_box * 3):
+                if given_number[x - 1][y - 1] in domain:
+                    domain.remove(int(given_number[x - 1][y - 1]))
+        return domain
+
 
 if __name__ == "__main__":
     root = Tk()
